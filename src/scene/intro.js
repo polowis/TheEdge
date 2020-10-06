d2a34c3f0cddf3d381c979db621f0568a9b93f22
polowis <tranphuochung0912@gmail.com>
2020-10-07 09:54:26 +1100

    fix: increase rain effect time intro so it touches the end of the screen before vanish

--- src/scene/intro.js
+++ src/scene/intro.js
@@ -12,11 +12,21 @@ export default class Intro extends Phaser.Scene{
 
     create() {
         this.bg = this.add.tileSprite(0, 0, 1280, 700, 'introbg').setOrigin(0)
+        this.createRainEffect()
+        this.title = this.add.text(this.cameras.main.worldView.x + this.cameras.main.width / 2, 200, 'THE EDGE', {fontSize: 30, fill: "red"}).setOrigin(0)
+        
+    }
+
+    update() {
+        this.bg.tilePositionX += 0.05;
+    }
+
+    createRainEffect() {
         this.rain = this.add.particles('raindrop');
         this.rain.createEmitter({
             x: { min: 1, max: 1800 },
             y: 0,
-            lifespan: 1200,
+            lifespan: 1300,
             speedY: { min: 200, max: 400 },
             gravityY: 100,
             gravityX: Phaser.Math.Between(100, 200),
@@ -24,11 +34,5 @@ export default class Intro extends Phaser.Scene{
             quantity: 4,
             blendMode: 'ADD'
         });
-
-        
-    }
-
-    update() {
-        this.bg.tilePositionX += 0.05;
     }
 }
